6cb82056ea51cf7577907830530de9d393a47eb7
brunocasu <brunocasu8@gmail.com>
2024-01-31 13:18:38 +0100

    ;fixed FTP server connection - must add  ftpSrv.setLocalIp(WiFi.softAPIP()); before starting ftp server

--- arduino-prj/monitor-ftp-ap-deep-sleep/monitor-ftp-ap-deep-sleep.ino
+++ arduino-prj/monitor-ftp-ap-deep-sleep/monitor-ftp-ap-deep-sleep.ino
@@ -11,15 +11,16 @@
 #include <PolledTimeout.h>
 #include <include/WiFiState.h>  // WiFiState structure details
 #include <LittleFS.h>
-#include <ESP8266FtpServer.h>
+//#include <ESP8266FtpServer.h>
 #include <Wire.h>
+#include <SimpleFTPServer.h>
 
 #define SERIAL_SPEED  115200
 
-// defines for the data aquisition system
-#define SHT30_I2C_ADDR_PIN_HIGH 0x45
-#define SHT30_I2C_ADDR_PIN_LOW 0x44
-#define SLEEP_TIME_MS 5000
+// Defines for the data aquisition system
+#define SHT30_I2C_ADDR_PIN_HIGH 0x45  // Jumper NOT connected
+#define SHT30_I2C_ADDR_PIN_LOW 0x44  // Jumper connected
+#define SLEEP_TIME_MS 5000  // Interval between measurements
 #define GPIO_SET_ACCESS_POINT D5 // On Wemos D1 Mini - Pin number 14 (GPIO14)
 
 // Remove when deploying in production environment
@@ -43,28 +44,30 @@ typedef enum {
 
 process_status_t process_status = PROCESS_DATA_AQUISITION;
 
-// SHT30 Shield
+// SHT30 configuration
 SHT3X sht30_1_handler(SHT30_I2C_ADDR_PIN_HIGH);
-sensor_status_t sht30_1_status = SENSOR_UNINITIALIZED;
-
-// SHT30 connect via cable in I2C port
 SHT3X sht30_2_handler(SHT30_I2C_ADDR_PIN_LOW);
+sensor_status_t sht30_1_status = SENSOR_UNINITIALIZED;
 sensor_status_t sht30_2_status = SENSOR_UNINITIALIZED;
 
-// Access Point configuration
+// WiFi Access Point configuration
 IPAddress local_IP(192,168,4,22);
 IPAddress gateway(192,168,4,9);
 IPAddress subnet_mask(255,255,255,0);
 const char* ssid_AP = "AP-ESP8266";
+
+// FTP configuration
 const char* user_FTP = "esp8266";
 const char* pwd_FTP = "esp8266";
 FtpServer ftpSrv;
 
 // Data file configuration
-File measurementsDataFile;
-const char* measurementsDataFileName = "esp8266Data.txt";
+//File sht30_1_DataFile;
+//File sht30_1_DataFile;
+const char* sht30_1_file_path = "/sht30_addr_45_data.csv";
+const char* sht30_2_file_path = "/sht30_addr_44_data.csv";
 
-ADC_MODE(ADC_VCC);
+// ADC_MODE(ADC_VCC);
 
 void setup() {
   // Init peripherals and GPIOs
@@ -72,29 +75,19 @@ void setup() {
   WiFi.forceSleepBegin();
   pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
   pinMode(GPIO_SET_ACCESS_POINT, INPUT_PULLUP);
-  Serial.begin(SERIAL_SPEED);
   Wire.begin();
-  
 #ifdef DEBUG_MODE
+  Serial.begin(SERIAL_SPEED);
   delay(200);
   Serial.println("\n***ESP8266 Temperature Monitor***");
   float result = ESP.getVcc();
-  Serial.print("VCC (V): ");
+  Serial.print("Input VCC (V): ");
   Serial.println((double) result * 0.001);
 #endif // DEBUG_MODE
   // Test SHT30 sensors
   sensorsInit();
   // Start File system
-  if (!LittleFS.begin()) {
-#ifdef DEBUG_MODE
-    Serial.println("\n-->LittleFS mount failed");
-#endif // DEBUG_MODE
-    return;
-  }
-  else{
-    Serial.println("\n-->LittleFS - Create Data File");
-    createDataFile();
-  }
+  initFS();
 }
 
 void loop() {
@@ -113,12 +106,16 @@ void loop() {
     setLED(BLUE_LED_OFF);
     readSensors();
     deviceSleep(SLEEP_TIME_MS);
+    listDir("/");
+    readFile(sht30_1_file_path);
+    delay(1000);
   }
   else if (process_status == PROCESS_ACCESS_POINT){
     setLED(BLUE_LED_ON);
     setAccessPoint();
   }
 }
+/** PF definitions **/
 
 void sensorsInit() {
   if(sht30_1_handler.get()==0){
@@ -147,7 +144,7 @@ void readSensors() {
     Serial.println(sht30_1_handler.humidity);
 #endif // DEBUG_MODE
     // Write SHT30 data into Data File
-    appendDataFile(sht30_1_handler.cTemp, sht30_1_handler.humidity, SHT30_I2C_ADDR_PIN_HIGH);
+    appendDataFile(sht30_1_handler.cTemp, sht30_1_handler.humidity, sht30_1_file_path);
   }
   else{
 #ifdef DEBUG_MODE
@@ -167,7 +164,7 @@ void readSensors() {
     Serial.println(sht30_2_handler.humidity);
 #endif // DEBUG_MODE
     // Write SHT30 data into Data File
-    appendDataFile(sht30_2_handler.cTemp, sht30_2_handler.humidity, SHT30_I2C_ADDR_PIN_LOW);
+    appendDataFile(sht30_2_handler.cTemp, sht30_2_handler.humidity, sht30_2_file_path);
   }
   else{
 #ifdef DEBUG_MODE
@@ -183,13 +180,14 @@ void setAccessPoint() {
   WiFi.softAPConfig(local_IP, gateway, subnet_mask);
   WiFi.softAP(ssid_AP); // No password for connecting
 #ifdef DEBUG_MODE
-  Serial.print("WiFi SSID: AP-ESP8266 (IP Addr): ");
+  Serial.print("\n-->WiFi SSID: AP-ESP8266 (IP Addr): ");
   Serial.println(WiFi.softAPIP());
 
   // Start FTP server
   Serial.println("FTP Server\nWaiting for connections...");
 #endif // DEBUG_MODE
   ftpSrv.begin(user_FTP, pwd_FTP);
+  ftpSrv.setLocalIp(WiFi.softAPIP());
   while (digitalRead(GPIO_SET_ACCESS_POINT) == LOW) {
     ftpSrv.handleFTP();
     delay(10);
@@ -199,39 +197,76 @@ void setAccessPoint() {
   WiFi.forceSleepBegin();
 }
 
-void createDataFile(){
-  if (LittleFS.exists(measurementsDataFileName)) {
-      Serial.print("Result file reported existing: ");
-      Serial.println(measurementsDataFileName);
-      measurementsDataFile = LittleFS.open(measurementsDataFileName, "r");
-      if (!measurementsDataFile) {
-        Serial.println("Unable to open result file for reading.");
-      } 
-      else {
-        Serial.print("File reported to be ");
-        Serial.print(measurementsDataFile.size());
-        Serial.println(" bytes in size.");
-      }
+void initFS() {
+  File new_file;
+#ifdef DEBUG_MODE
+  Serial.println("\n-->LittleFS start");
+  LittleFS.format();
+#endif // DEBUG_MODE
+  if (!LittleFS.begin()) {
+    // LittleFS mount failed
+    Serial.println("\n-->LittleFS mount failed");
+    return;
   }
-  else {
-    Serial.print("Result file reported NOT existing. Proceeding to create a new one: ");
-    Serial.println(measurementsDataFileName);
+  else{
+    // Create file for SHT30 Addr 45 data // TODO remove seral prints
+    new_file = LittleFS.open(sht30_1_file_path, "w");
+    if (!new_file) {
+      Serial.println("Failed to open file for writing");
+      return;
+    }
+    if (new_file.print("Temperature(C);RelHumidity(RH%)")) {
+      Serial.println("File written");
+    } else {
+      Serial.println("Write failed");
+    }
+    new_file.close();
   }
 }
 
-void appendDataFile(float temp, float r_hum, char i2c_addr){
-  measurementsDataFile = LittleFS.open(measurementsDataFileName, "a");
-    if (!measurementsDataFile) {
-      Serial.println("Unable to open result file for appending.");
+void appendDataFile(float temp, float r_hum, const char *path) {
+  //sht30_1_DataFile = LittleFS.open(sht30_1_file_path, "a");
+  File data_file = LittleFS.open(path, "a");
+    if (!data_file) {
+      Serial.print("Unable to open file");
+      Serial.println(path);
     } else {
-      measurementsDataFile.print(temp, 2);
-      measurementsDataFile.print(";");
-      measurementsDataFile.print(r_hum, 2);
-      measurementsDataFile.print(";");
-      measurementsDataFile.print(i2c_addr, HEX);
-      measurementsDataFile.flush();
+      data_file.print("\n");
+      data_file.print(temp, 2);
+      data_file.print(";");
+      data_file.print(r_hum, 2);
+      //data_file.flush();
     }
-    measurementsDataFile.close();
+    data_file.close();
+}
+
+void readFile(const char *path) {
+  Serial.printf("\nReading file: %s\n", path);
+
+  File file = LittleFS.open(path, "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.print("Read from file:\n");
+  while (file.available()) { Serial.write(file.read()); }
+  file.close();
+}
+
+void listDir(const char *dirname) {
+  Serial.printf("\nListing directory: %s\n", dirname);
+
+  Dir root = LittleFS.openDir(dirname);
+
+  while (root.next()) {
+    File file = root.openFile("r");
+    Serial.print("  FILE: ");
+    Serial.print(root.fileName());
+    Serial.print("  SIZE: ");
+    Serial.print(file.size());
+    file.close();
+  }
 }
 
 void deviceSleep(int sleep_time) {
@@ -244,7 +279,7 @@ void deviceSleep(int sleep_time) {
 #endif // DEBUG_MODE
 }
 
-void setLED(led_status_t cmd){
+void setLED(led_status_t cmd) {
   if(cmd == BLUE_LED_ON){
     digitalWrite(LED_BUILTIN, LOW);
   }
