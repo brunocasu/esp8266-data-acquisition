1acb16bd104ab0595edd80e80b23a8d8dbf44aaa
brunocasu <brunocasu8@gmail.com>
2024-05-16 18:58:07 +0200

    added project for pressure sensor - edited LOLIN_HP303B lib

--- esp8266-arduino-prj/app-ftp-ap-sht30-hp303b/app-ftp-ap-sht30-hp303b.ino
+++ esp8266-arduino-prj/app-ftp-ap-sht30-hp303b/app-ftp-ap-sht30-hp303b.ino
@@ -1,5 +1,5 @@
 /*
- * app-ftp-ap-sht30.ino
+ * app-ftp-ap-sht30-hp3038.ino
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of
@@ -12,10 +12,10 @@
  * You should have received a copy of the GNU General Public License along with this program. If not,
  * see <https://www.gnu.org/licenses/>.
  *
- *  Created on: Jan 25, 2024
+ *  Created on: May 16, 2024
  *      Author: Bruno Casu
  *
- *  Version 1.1 (Apr 10, 2024)
+ *  Version 1.0 (May 16, 2024)
  */
  
 #include <ESP8266WiFi.h>
@@ -26,18 +26,20 @@
 #include <SimpleFTPServer.h> // Replacing <ESP8266FtpServer.h>
 #include <cozir.h>
 #include <sht30.h>
+#include <LOLIN_HP303B.h>
 
 #define SERIAL_SPEED  115200
-#define APP_VERSION "app-ftp-ap-sht30 V1.1"
+#define APP_VERSION "app-ftp-ap-sht30-hp3038_v1.1"
 
 // Defines for the data aquisition system
 #define SHT30_I2C_ADDR_PIN_HIGH 0x45  // Jumper NOT connected
 #define SHT30_I2C_ADDR_PIN_LOW 0x44  // Jumper connected
+#define HP303B_I2C_ADDR_PIN_HIGH 0x77  // Jumper NOT connected
 #define SLEEP_TIME_MS 1800000  // Interval between measurements in ms
 #define GPIO_SET_ACCESS_POINT 14 // On Wemos D1 Mini - Pin number 14 (GPIO14)
 
 // Remove when deploying in production environment
-//#define DEBUG_MODE
+#define DEBUG_MODE
 
 #define CYCLE_COUNTER_RST 10000000  // When resetig via GPIO, RTC memory gets wrong counter value - must be reset
 unsigned long cycle_counter = 0;
@@ -45,6 +47,8 @@ unsigned long cycle_counter = 0;
 // Temperature and humidity sensor
 SHT30 sht30_1_handler(SHT30_I2C_ADDR_PIN_HIGH); // Addr 0x45
 SHT30 sht30_2_handler(SHT30_I2C_ADDR_PIN_LOW); // Addr 0x44
+// Temperature and pressure sensor
+LOLIN_HP303B hp303b_handler;
 
 // WiFi Access Point configuration
 IPAddress local_IP(10,10,10,1); // FTP server address
@@ -63,26 +67,44 @@ FSInfo fs_info;
 // Data file configuration
 const char* sht30_1_file_path = "/sht30_addr_45_data.csv";
 const char* sht30_2_file_path = "/sht30_addr_44_data.csv";
-const char* sht30_csv_header_description = "Ctr;Temperature(C);RelHumidity(RH%)"; // Added at the creation of the Data file
+const char* hp303b_file_path = "/hp303b_addr_77_data.csv";
+const char* sht30_csv_header_description = "Ctr;Temperature(C);RelHumidity(RH%);ExeTime(ms)"; // Added at the creation of the Data file
+const char* hp303b_csv_header_description = "Ctr;Temperature(C);Pressure(Pa);ExeTime(ms)"; // Added at the creation of the Data file
 
 
 /** PF definitions **/
 
 /**
- * Configures the ESP8266 as WiFi Access Point, and starts the FTP server
+ * Error handler function
  */
-void setAccessPoint() {
+void errorHandler(){
+  for(;;){
+    digitalWrite(LED_BUILTIN, LOW); // LED on
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH); // LED off
+    delay(100);
+  }
+}
+
+/**
+ * Serial print the device configuration and file system info
+ */
+void printDeviceInfo(){
+  LittleFS.info(fs_info);
   Serial.begin(SERIAL_SPEED);
-  delay(100);
-  // Configure WiFi Access point
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid_AP); // No password for connecting
-  WiFi.softAPConfig(local_IP, gateway, subnet_mask);
-  // Device Info
-  Serial.print("\n-->SW VERSION: ");
+  delay(500);
+  Serial.print("\n\n-->SW VERSION: ");
   Serial.println(APP_VERSION);
+  Serial.print(F("Cycle counter = "));
+  Serial.println(cycle_counter);
   Serial.print("\n-->SLEEP_TIME_MS: ");
   Serial.println(SLEEP_TIME_MS);
+  // FS info
+  Serial.print("-->LittleFS Mount OK\nTotal FS Size (kB):");
+  Serial.println(fs_info.totalBytes*0.001);
+  Serial.print("Used (kB): ");
+  Serial.println(fs_info.usedBytes*0.001);
+  // FTP-AP info
   Serial.print("\n-->AP Mode - WiFi SSID (NO PASSWORD): ");
   Serial.println(ssid_AP);
   Serial.print("\n-->FTP Server IP Addr: ");
@@ -92,7 +114,19 @@ void setAccessPoint() {
   Serial.print("FTP_PWD: ");
   Serial.println(pwd_FTP);
   Serial.println("\n-->Waiting for connections...");
+  
+  Serial.flush();
+}
 
+/**
+ * Configures the ESP8266 as WiFi Access Point, and starts the FTP server
+ */
+void setAccessPoint() {
+  // Configure WiFi Access point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid_AP); // No password for connecting
+  WiFi.softAPConfig(local_IP, gateway, subnet_mask);
+  printDeviceInfo();
   // Start FTP server
   ftpSrv.begin(user_FTP, pwd_FTP);
   ftpSrv.setLocalIp(WiFi.softAPIP()); // Must setLocalIP as the same as the one in softAPConfig()
@@ -106,25 +140,7 @@ void setAccessPoint() {
 }
 
 /**
- * Start LittleFS
- */
-void mountLittleFS(){
-  if (!LittleFS.begin()) {
-    return; // LittleFS mount failed
-  }
-#ifdef DEBUG_MODE
-  LittleFS.info(fs_info);
-  Serial.print("-->LittleFS Mount OK\nTotal FS Size (kB):");
-  Serial.println(fs_info.totalBytes*0.001);
-  Serial.print("Used (kB): ");
-  Serial.println(fs_info.usedBytes*0.001);
-#endif // DEBUG_MODE
-}
-
-/**
- * Check what sensors are connected at first boot, and create files for each sensor
- * If sensor is not present at first boot, the file is not created, and following measurements are not taken
- * This prevents polling inactive of faulty sensors
+ * Create data files for each sensor
  */
 void createDataFiles(){
   File new_file;
@@ -132,12 +148,9 @@ void createDataFiles(){
   if (!LittleFS.exists(sht30_1_file_path)){ // File does not exist
     new_file = LittleFS.open(sht30_1_file_path, "w");
     if (!new_file) {
-      return; // Failed creating file
+      errorHandler(); // Failed creating file
     }
     else{
-      new_file.print("[SleepTime(s)=");
-      new_file.print(SLEEP_TIME_MS/1000);
-      new_file.print("]");
       new_file.print(sht30_csv_header_description); // Write CSV header
       new_file.close();
     }
@@ -145,13 +158,20 @@ void createDataFiles(){
   if (!LittleFS.exists(sht30_2_file_path)){ // File does not exist
     new_file = LittleFS.open(sht30_2_file_path, "w");
     if (!new_file) {
-      return; // Failed creating file
+      errorHandler(); // Failed creating file
     }
     else{
-      new_file.print("[SleepTime(s)=");
-      new_file.print(SLEEP_TIME_MS/1000);
-      new_file.print("]");
       new_file.print(sht30_csv_header_description); // Write CSV header
+      new_file.close();
+    }
+  }
+    if (!LittleFS.exists(hp303b_file_path)){ // File does not exist
+    new_file = LittleFS.open(hp303b_file_path, "w");
+    if (!new_file) {
+      errorHandler(); // Failed creating file
+    }
+    else{
+      new_file.print(hp303b_csv_header_description); // Write CSV header
       new_file.close();
     }
   }
@@ -161,8 +181,22 @@ void createDataFiles(){
  * Read and save sensor data on data files
  */
 void dataAcquisition() {
+  double hp303b_temp;
+  int32_t temp; // not used
+  int32_t hp303b_pres;
+  int16_t hp303b_oversampling = 2; // Highest precision
+  
   sht30_1_handler.cTemp = 0;
   sht30_2_handler.cTemp = 0;
+  if (LittleFS.exists(hp303b_file_path)){ // File exists
+    hp303b_handler.begin();
+    if(hp303b_handler.measureTempOnce(temp, hp303b_oversampling) == 0){ // Temperature reading OK
+      if(hp303b_handler.measurePressureOnce(hp303b_pres, hp303b_oversampling) == 0){ // Pressure reading OK
+        hp303b_temp = hp303b_handler.returnDoublePrecisionTemp(); // Adapted function to retrieve decimal precision temperature (LOLIN_HP303B driver returns int32_t)
+        appendHP303BData(HP303B_I2C_ADDR_PIN_HIGH, hp303b_temp, hp303b_pres, hp303b_file_path); // Append sensor data
+      }
+    }
+  }
   if (LittleFS.exists(sht30_1_file_path)){ // File exists
     if(sht30_1_handler.read_single_shot() == SHT30_READ_OK){
       appendSHT30Data(SHT30_I2C_ADDR_PIN_HIGH, sht30_1_handler.cTemp, sht30_1_handler.humidity, sht30_1_file_path); // Append sensor data
@@ -173,12 +207,14 @@ void dataAcquisition() {
       appendSHT30Data(SHT30_I2C_ADDR_PIN_LOW, sht30_2_handler.cTemp, sht30_2_handler.humidity, sht30_2_file_path); // Append sensor data
     }
   }
+
 }
 
 /**
  * Append the SHT30 data into the Data files
  */
 void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path) {
+  int exe_time = millis();
   File data_file = LittleFS.open(path, "a");
     if (!data_file) {
       return;
@@ -190,6 +226,8 @@ void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path
       data_file.print(temp, 2);
       data_file.print(";");
       data_file.print(r_hum, 2);
+      data_file.print(";");
+      data_file.print(exe_time);
       data_file.flush(); // Ensure writting before returning
     }
     data_file.close();
@@ -200,51 +238,65 @@ void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path
     Serial.print(temp);
     Serial.print(" RH(RH%): ");
     Serial.print(r_hum);
+    Serial.print(" ExeTime(ms): ");
+    Serial.print(exe_time);
 #endif // DEBUG_MODE
 }
 
 /**
- * Append the execution time (obtained by the return value of the millis() functions)
+ * Append the SHT30 data into the Data files
  */
-void appendExecutionTime(int exe_time, const char *path) {
-
+void appendHP303BData(uint8_t i2c_addr, double temp, int32_t pres, const char *path) {
+  int exe_time = millis();
   File data_file = LittleFS.open(path, "a");
     if (!data_file) {
       return;
     }
     else {
+      data_file.print("\n");
+      data_file.print(cycle_counter);
+      data_file.print(";");
+      data_file.print(temp, 2);
+      data_file.print(";");
+      data_file.print(pres);
       data_file.print(";");
       data_file.print(exe_time);
       data_file.flush(); // Ensure writting before returning
     }
     data_file.close();
+#ifdef DEBUG_MODE
+    Serial.print("\n-->HP303B ");
+    Serial.print(i2c_addr, HEX);
+    Serial.print(" T(C): ");
+    Serial.print(temp);
+    Serial.print(" P(Pa): ");
+    Serial.print(pres);
+    Serial.print(" ExeTime(ms): ");
+    Serial.print(exe_time);
+#endif // DEBUG_MODE
 }
+
 
 void setup() {
   system_rtc_mem_read(64, &cycle_counter, 4); // Copy RTC memory value in current cycle counter
   // When reset by GPIO RTC memeory gets wrong value, this prevents errors in first boot if done by GPIO reset
   if (cycle_counter > CYCLE_COUNTER_RST){cycle_counter = 0;}
-#ifdef DEBUG_MODE
-  Serial.begin(SERIAL_SPEED);
-  delay(500);
-  Serial.println("\n\n*** SETUP ***");
-  Serial.print("-->SW VERSION: ");
-  Serial.println(APP_VERSION);
-  Serial.print(F("Number of Cycles executed = "));
-  Serial.println(cycle_counter);
-  Serial.flush();
-#endif // DEBUG_MODE
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // LED off
   pinMode(GPIO_SET_ACCESS_POINT, INPUT_PULLUP); // Set Access point pin
   Wire.begin(); // I2C
   WiFi.mode(WIFI_OFF); // Must turn the modem off; using disconnect won't work
   WiFi.forceSleepBegin();
+  // Mount file system
+  if (!LittleFS.begin()) {
+    errorHandler(); // LittleFS mount failed
+  }
+#ifdef DEBUG_MODE
+  printDeviceInfo();
+#endif // DEBUG_MODE  
 
-  mountLittleFS();
-
+  // Setup data files and read the sensors
   createDataFiles();
-
   dataAcquisition();
   
   // Increment and save the current cycle counter value in RTC memory
@@ -258,22 +310,7 @@ void setup() {
     setAccessPoint();
   }
 
-  int exe_time = millis();
-  //if (sht30_1_handler.cTemp>0){appendExecutionTime(exe_time, sht30_1_file_path);}
-  //if (sht30_2_handler.cTemp>0)appendExecutionTime(exe_time, sht30_2_file_path);
-
-#ifdef DEBUG_MODE
-  Serial.print("\n-->Execution time (ms): ");
-  Serial.print(exe_time);
-  Serial.print(F("\n-->Device will deep Sleep for (ms): "));
-  Serial.println(SLEEP_TIME_MS);
-  LittleFS.info(fs_info);
-  Serial.print("-->LittleFS End of Execution\nTotal FS Size (kB):");
-  Serial.println(fs_info.totalBytes*0.001);
-  Serial.print("Used (kB): ");
-  Serial.println(fs_info.usedBytes*0.001);
-  Serial.flush();
-#endif // DEBUG_MODE
+  // Sleep untile next cycle
   ESP.deepSleep(SLEEP_TIME_MS*1000, WAKE_NO_RFCAL); // Deep Sleep - MCU reset at wakeup - GPIO16 must be connected to RST
 }
 
---
+++ esp8266-arduino-prj/app-ftp-ap-sht30/app-ftp-ap-sht30.ino
@@ -0,0 +1,256 @@
+/*
+ * app-ftp-ap-sht30.ino
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see <https://www.gnu.org/licenses/>.
+ *
+ *  Created on: Jan 25, 2024
+ *      Author: Bruno Casu
+ *
+ *  Version 1.1 (May 16, 2024)
+ */
+ 
+#include <ESP8266WiFi.h>
+#include <PolledTimeout.h>
+#include <LittleFS.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <SimpleFTPServer.h> // Replacing <ESP8266FtpServer.h>
+#include <cozir.h>
+#include <sht30.h>
+
+#define SERIAL_SPEED  115200
+#define APP_VERSION "app-ftp-ap-sht30_v1.1"
+
+// Defines for the data aquisition system
+#define SHT30_I2C_ADDR_PIN_HIGH 0x45  // Jumper NOT connected
+#define SHT30_I2C_ADDR_PIN_LOW 0x44  // Jumper connected
+#define SLEEP_TIME_MS 1800000  // Interval between measurements in ms
+#define GPIO_SET_ACCESS_POINT 14 // On Wemos D1 Mini - Pin number 14 (GPIO14)
+
+// Remove when deploying in production environment
+//#define DEBUG_MODE
+
+#define CYCLE_COUNTER_RST 10000000  // When resetig via GPIO, RTC memory gets wrong counter value - must be reset
+unsigned long cycle_counter = 0;
+
+// Temperature and humidity sensor
+SHT30 sht30_1_handler(SHT30_I2C_ADDR_PIN_HIGH); // Addr 0x45
+SHT30 sht30_2_handler(SHT30_I2C_ADDR_PIN_LOW); // Addr 0x44
+
+// WiFi Access Point configuration
+IPAddress local_IP(10,10,10,1); // FTP server address
+IPAddress gateway(10,10,10,1);
+IPAddress subnet_mask(255,255,255,0);
+const char* ssid_AP = "AP-ESP8266"; // No password set
+
+// FTP server access configuration
+const char* user_FTP = "esp8266";
+const char* pwd_FTP = "esp8266";
+FtpServer ftpSrv; // Handler
+
+// LittleFs info
+FSInfo fs_info;
+
+// Data file configuration
+const char* sht30_1_file_path = "/sht30_addr_45_data.csv";
+const char* sht30_2_file_path = "/sht30_addr_44_data.csv";
+const char* sht30_csv_header_description = "Ctr;Temperature(C);RelHumidity(RH%);ExeTime(ms)"; // Added at the creation of the Data file
+
+
+/** PF definitions **/
+
+/**
+ * Error handler function
+ */
+void errorHandler(){
+  for(;;){
+    digitalWrite(LED_BUILTIN, LOW); // LED on
+    delay(100);
+    digitalWrite(LED_BUILTIN, HIGH); // LED off
+    delay(100);
+  }
+}
+
+/**
+ * Serial print the device configuration and file system info
+ */
+void printDeviceInfo(){
+  LittleFS.info(fs_info);
+  Serial.begin(SERIAL_SPEED);
+  delay(500);
+  Serial.print("\n\n-->SW VERSION: ");
+  Serial.println(APP_VERSION);
+  Serial.print(F("Cycle counter = "));
+  Serial.println(cycle_counter);
+  Serial.print("\n-->SLEEP_TIME_MS: ");
+  Serial.println(SLEEP_TIME_MS);
+  // FS info
+  Serial.print("-->LittleFS Mount OK\nTotal FS Size (kB):");
+  Serial.println(fs_info.totalBytes*0.001);
+  Serial.print("Used (kB): ");
+  Serial.println(fs_info.usedBytes*0.001);
+  // FTP-AP info
+  Serial.print("\n-->AP Mode - WiFi SSID (NO PASSWORD): ");
+  Serial.println(ssid_AP);
+  Serial.print("\n-->FTP Server IP Addr: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("Plain FTP Connection (insecure)\nFTP_USR: ");
+  Serial.println(user_FTP);
+  Serial.print("FTP_PWD: ");
+  Serial.println(pwd_FTP);
+  Serial.println("\n-->Waiting for connections...");
+  
+  Serial.flush();
+}
+
+/**
+ * Configures the ESP8266 as WiFi Access Point, and starts the FTP server
+ */
+void setAccessPoint() {
+  // Configure WiFi Access point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid_AP); // No password for connecting
+  WiFi.softAPConfig(local_IP, gateway, subnet_mask);
+  printDeviceInfo();
+  // Start FTP server
+  ftpSrv.begin(user_FTP, pwd_FTP);
+  ftpSrv.setLocalIp(WiFi.softAPIP()); // Must setLocalIP as the same as the one in softAPConfig()
+  while (digitalRead(GPIO_SET_ACCESS_POINT) == LOW) {
+    ftpSrv.handleFTP();
+    delay(10);
+  }
+  WiFi.softAPdisconnect(true);
+  WiFi.mode(WIFI_OFF);
+  WiFi.forceSleepBegin();
+}
+
+/**
+ * Create data files for each sensor
+ */
+void createDataFiles(){
+  File new_file;
+  // Create Data files, if they do not exist
+  if (!LittleFS.exists(sht30_1_file_path)){ // File does not exist
+    new_file = LittleFS.open(sht30_1_file_path, "w");
+    if (!new_file) {
+      errorHandler(); // Failed creating file
+    }
+    else{
+      new_file.print(sht30_csv_header_description); // Write CSV header
+      new_file.close();
+    }
+  }
+  if (!LittleFS.exists(sht30_2_file_path)){ // File does not exist
+    new_file = LittleFS.open(sht30_2_file_path, "w");
+    if (!new_file) {
+      errorHandler(); // Failed creating file
+    }
+    else{
+      new_file.print(sht30_csv_header_description); // Write CSV header
+      new_file.close();
+    }
+  }
+}
+
+/**
+ * Read and save sensor data on data files
+ */
+void dataAcquisition() {
+  sht30_1_handler.cTemp = 0;
+  sht30_2_handler.cTemp = 0;
+  if (LittleFS.exists(sht30_1_file_path)){ // File exists
+    if(sht30_1_handler.read_single_shot() == SHT30_READ_OK){
+      appendSHT30Data(SHT30_I2C_ADDR_PIN_HIGH, sht30_1_handler.cTemp, sht30_1_handler.humidity, sht30_1_file_path); // Append sensor data
+    }
+  }
+  if (LittleFS.exists(sht30_2_file_path)){ // File exists
+    if(sht30_2_handler.read_single_shot() == SHT30_READ_OK){
+      appendSHT30Data(SHT30_I2C_ADDR_PIN_LOW, sht30_2_handler.cTemp, sht30_2_handler.humidity, sht30_2_file_path); // Append sensor data
+    }
+  }
+}
+
+/**
+ * Append the SHT30 data into the Data files
+ */
+void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path) {
+  int exe_time = millis();
+  File data_file = LittleFS.open(path, "a");
+    if (!data_file) {
+      return;
+    }
+    else {
+      data_file.print("\n");
+      data_file.print(cycle_counter);
+      data_file.print(";");
+      data_file.print(temp, 2);
+      data_file.print(";");
+      data_file.print(r_hum, 2);
+      data_file.print(";");
+      data_file.print(exe_time);
+      data_file.flush(); // Ensure writting before returning
+    }
+    data_file.close();
+#ifdef DEBUG_MODE
+    Serial.print("\n-->SHT30 ");
+    Serial.print(i2c_addr, HEX);
+    Serial.print(" T(C): ");
+    Serial.print(temp);
+    Serial.print(" RH(RH%): ");
+    Serial.print(r_hum);
+    Serial.print(" ExeTime(ms): ");
+    Serial.print(exe_time);
+#endif // DEBUG_MODE
+}
+
+
+void setup() {
+  system_rtc_mem_read(64, &cycle_counter, 4); // Copy RTC memory value in current cycle counter
+  // When reset by GPIO RTC memeory gets wrong value, this prevents errors in first boot if done by GPIO reset
+  if (cycle_counter > CYCLE_COUNTER_RST){cycle_counter = 0;}
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // LED off
+  pinMode(GPIO_SET_ACCESS_POINT, INPUT_PULLUP); // Set Access point pin
+  Wire.begin(); // I2C
+  WiFi.mode(WIFI_OFF); // Must turn the modem off; using disconnect won't work
+  WiFi.forceSleepBegin();
+  // Mount file system
+  if (!LittleFS.begin()) {
+    errorHandler(); // LittleFS mount failed
+  }
+#ifdef DEBUG_MODE
+  printDeviceInfo();
+#endif // DEBUG_MODE  
+for(int k=0;k<10000;k++){
+  //delay(1000);
+  // Setup data files and read the sensors
+  createDataFiles();
+  dataAcquisition();
+  
+  // Increment and save the current cycle counter value in RTC memory
+  cycle_counter++;
+  system_rtc_mem_write(64, &cycle_counter, 4);
+
+  if (digitalRead(GPIO_SET_ACCESS_POINT) == LOW){ // Change to AP mode
+    digitalWrite(LED_BUILTIN, LOW); // LED on
+    cycle_counter = 0;
+    system_rtc_mem_write(64, &cycle_counter, 4); // Reset the cycle counter
+    setAccessPoint();
+  }
+}
+errorHandler();
+  // Sleep untile next cycle
+  ESP.deepSleep(SLEEP_TIME_MS*1000, WAKE_NO_RFCAL); // Deep Sleep - MCU reset at wakeup - GPIO16 must be connected to RST
+}
+
+
+void loop() { }
