74f17ead34b7cd961decde9e6da6d9afd07b80c3
brunocasu <brunocasu8@gmail.com>
2024-06-04 01:34:36 +0200

    espm2 app for temperature and pressure v1.0 - debug mode

--- esp8285-arduino-prj/app-fast-reading/app-fast-reading.ino
+++ esp8285-arduino-prj/app-fast-reading/app-fast-reading.ino
@@ -33,6 +33,22 @@ void errorHandler(){
 }
 
 /**
+ * 
+ * 
+ */
+int ms5803_send_cmd(uint8_t cmd){
+  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
+  Wire.write(cmd);
+  delay(3);
+  if (Wire.endTransmission() == 0) {
+    return 0; // Command response OK
+  }
+  else {
+    return -1; // Error in I2C transmission
+  }
+}
+
+/**
  * Initialyze the MS5803 sensor: configures the GPIOs and send a Reset command
  * Return 0 if command sent is OK, and -1 if Error
  */
@@ -41,17 +57,15 @@ int ms5803_init(void){
   digitalWrite(MS5803_VCC_PIN, HIGH); // Must be set to high to power the ms503 sensor
   pinMode(GPIO_2_VCC, OUTPUT);
   digitalWrite(GPIO_2_VCC, HIGH);
-
+  //delay(100);
   Wire.setClock(100000UL);
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN, I2C_CONTROLLER_ADDR);
   // Send a RESET command
-  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
-  Wire.write(CMD_RESET);
-  if (Wire.endTransmission() != 0) {
-    return -1; // Error in I2C transmission
+  if (ms5803_send_cmd(CMD_RESET) != MS5803_CMD_OK){
+    return -1;
   }
-  else {
-    return 0; // Command response OK
+  else{
+    return 0;
   }
 }
 
@@ -63,32 +77,23 @@ int ms5803_read_coefficients(uint16_t *coef){
   int n;
   uint8_t buff[2];
   for (int i = 0; i < 8; i++) { // Request values for 8 coefficients
-    Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
-    Wire.write((uint8_t) (CMD_RDROM + (i << 1)));
-    if (Wire.endTransmission() != 0) {
+    if (ms5803_send_cmd((uint8_t) (CMD_RDROM + (i << 1))) != MS5803_CMD_OK){
+      return -1;
+    }
+    n = 0;
+    buff[0]=0;
+    buff[1]=0;
+    Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 2); // Request 2 bytes from device
+    delay(1);
+    while (Wire.available() && n < 2) { // Device may send less than requested
+      buff[n] = Wire.read();
+      n++;
+    }
+    if (n != 2) {
       return -1; // Error in I2C transmission
     }
-    else { // Read ROM command OK - read coeficients
-      //delay(5);
-      n = 0;
-      buff[0]=0;
-      buff[1]=0;
-      Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
-      Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 2); // Request 2 bytes from device
-      if (Wire.endTransmission() != 0) {
-        return -1; // Error in I2C transmission
-      }
-      while (Wire.available() && n < 2) { // Device may send less than requested
-        buff[n] = Wire.read();
-        n++;
-      }
-      if (n != 2) {
-        return -1; // Error in I2C transmission
-      }
-      else{ // Copy 16 bit coefficient
-        coef[i] = (uint16_t)buff[1] | ((uint16_t)buff[0] << 8);
-      }
-    }
+    // Copy 16 bit coefficient
+    coef[i] = (uint16_t)buff[1] | ((uint16_t)buff[0] << 8);
     delay(5);
   }
   return 0;
@@ -98,41 +103,76 @@ int ms5803_read_coefficients(uint16_t *coef){
  * Send command to trigger the ADC conversion then request the Raw data (3 bytes)
  * Return raw data value
  */
-unsigned long ms5803_read_raw_pressure(void){
+int ms5803_read_raw_pressure(unsigned long *raw_pressure){
   int n;
-  uint8_t buff[2];
+  uint8_t buff[3];
+  *raw_pressure = 0;
   // Send Conversion request for Pressure
-  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
-  Wire.write(CMD_CONVP);
-  if (Wire.endTransmission() != 0) {
-    return -1; // Error in I2C transmission
+  if (ms5803_send_cmd(CMD_CONVP) != MS5803_CMD_OK){
+    return -1;
   }
-  delay(3);
+  
+  delay(3); // Delay for conversion
+  
   // Send read ADC command
-  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
-  Wire.write(CMD_RDADC);
-  if (Wire.endTransmission() != 0) {
-    return -1; // Error in I2C transmission
+  if (ms5803_send_cmd(CMD_RDADC) != MS5803_CMD_OK){
+    return -1;
   }
-  // Request 3 bytes of conversion results
-  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
+  
   Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 3);
-  if (Wire.endTransmission() != 0) {
+  delay(1);
+  n = 0;
+  while (Wire.available() && n < 3) { // slave may send less than requested
+    buff[n] = Wire.read(); // receive a byte as character
+    *raw_pressure = (*raw_pressure << 8) + (uint8_t) (buff[n] & 0xff);
+    n++;
+  }
+  if (n != 3) {
     return -1; // Error in I2C transmission
   }
-  // Actually read the 3 bytes of data back and rearrange them
-  result = Wire.available();
-  Serial.print("Pressure read request gave ");
-  Serial.println(result);
-  unsigned long iPressure = 0;
-  jndex = 0;
-  while (Wire.available() && jndex < 3) { // slave may send less than requested
-    press_buff[jndex] = Wire.read(); // receive a byte as character
-    iPressure = (iPressure << 8) + (uint8_t) (press_buff[jndex] & 0xff);
-    Serial.println(press_buff[jndex]);
-    jndex++;
+  else{
+    return 0; // Command OK
   }
+
 }
+
+/**
+ * Send command to trigger the ADC conversion then request the Raw data (3 bytes)
+ * Return raw data value
+ */
+int ms5803_read_raw_temperature(unsigned long *raw_temperature){
+  int n;
+  uint8_t buff[3];
+  *raw_temperature = 0;
+  // Send Conversion request for Pressure
+  if (ms5803_send_cmd(CMD_CONVT) != MS5803_CMD_OK){
+    return -1;
+  }
+  
+  delay(3); // Delay for conversion
+  
+  // Send read ADC command
+  if (ms5803_send_cmd(CMD_RDADC) != MS5803_CMD_OK){
+    return -1;
+  }
+  
+  Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 3);
+  delay(1);
+  n = 0;
+  while (Wire.available() && n < 3) { // slave may send less than requested
+    buff[n] = Wire.read(); // receive a byte as character
+    *raw_temperature = (*raw_temperature << 8) + (uint8_t) (buff[n] & 0xff);
+    n++;
+  }
+  if (n != 3) {
+    return -1; // Error in I2C transmission
+  }
+  else{
+    return 0; // Command OK
+  }
+
+}
+
 
 
 void setup() {
@@ -141,29 +181,55 @@ void setup() {
   uint8_t serial_buff[2];
   uint8_t press_buff[3];
   int jndex;
+  unsigned long raw_pressure = 0;
 
-  Serial.println("Mount LittleFS");
-  if (!LittleFS.begin()) {
-    Serial.println("LittleFS mount failed");
-    return;
+  pinMode(MS5803_VCC_PIN, OUTPUT);
+  digitalWrite(MS5803_VCC_PIN, HIGH);
+  pinMode(GPIO_2_VCC, OUTPUT);
+  digitalWrite(GPIO_2_VCC, HIGH);
+  //Serial.println("Mount LittleFS");
+  //if (!LittleFS.begin()) {
+  //  Serial.println("LittleFS mount failed");
+  //  return;
+  //}
+  //Serial.println("Formatting LittleFS filesystem");
+  //LittleFS.format();
+  //
+  if(ms5803_init() != MS5803_CMD_OK){
+    errorHandler();
   }
-  Serial.println("Formatting LittleFS filesystem");
-  LittleFS.format();
+  Serial.println("\nMS5803 INIT OK");
   
+  if(ms5803_read_coefficients(coef_buff) != MS5803_CMD_OK){
+    errorHandler();
+  }
+  Serial.println("COEFFICIENTS");
+  for(int k=0;k<8;k++){
+    Serial.print(k);
+    Serial.print("-");
+    Serial.println(coef_buff[k]);
+  }
+  
+  Serial.println("RAW PRESSURE READINGS");
+  if(ms5803_read_raw_pressure(&raw_pressure) != MS5803_CMD_OK){
+    errorHandler();
+  }
+  
+  Serial.println(raw_pressure);
   //writeFile("/test_pressure_temp_data.txt", "INIT\n");
   //appendFile("/hello.txt", "World!\n");
 
-  // Setup I2C for reading out the pressure sensor
+  //Setup I2C for reading out the pressure sensor
   //Wire.setClock(100000UL);
-  //Wire.begin(SDA_PIN, SCL_PIN, I2C_MASTER);        // join i2c bus (address optional for master)
+  //Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN, I2C_CONTROLLER_ADDR);        // join i2c bus (address optional for master)
 
   // Send a RESET command
-  //Wire.beginTransmission(I2C_SLAVE);
+  //Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
   //Wire.write(CMD_RESET);
-  // delay(3);
+  //delay(3);
   //if (Wire.endTransmission() == 0) {
   //  Serial.print("SLAVE found at address ");
-  //  Serial.println(I2C_SLAVE, HEX);
+  //  Serial.println(MS5803_I2C_DEVICE_ADDR, HEX);
   //}
   //delay(50);
   //for (int i = 0; i < 8; i++) {
@@ -194,32 +260,46 @@ void setup() {
   //system_rtc_mem_write(rtcPos, rCoeff, 8 * 8);
 
 // Now read the pressure: send a conversion command
-    Wire.beginTransmission(I2C_SLAVE);
-    Wire.write(CMD_CONVP);
-    int result = Wire.endTransmission();
-    delay(3);
-    // Send a command to read the ADC
-    Wire.beginTransmission(I2C_SLAVE);
-    Wire.write(CMD_RDADC);
-    result = Wire.endTransmission();
-    // Request 3 bytes of conversion results
-    Wire.beginTransmission(I2C_SLAVE);
-    Wire.requestFrom(I2C_SLAVE, 3);
-    result = Wire.endTransmission();
-    // Actually read the 3 bytes of data back and rearrange them
-    result = Wire.available();
-    Serial.print("Pressure read request gave ");
-    Serial.println(result);
-    unsigned long iPressure = 0;
-    jndex = 0;
-    while (Wire.available()&& jndex<3) { // slave may send less than requested
-      press_buff[jndex] = Wire.read(); // receive a byte as character
-      iPressure = (iPressure << 8) + (uint8_t) (press_buff[jndex] & 0xff);
-      Serial.println(press_buff[jndex]);
-      jndex++;
-    }
+  //Wire.beginTransmission(I2C_SLAVE);
+  //Wire.write(CMD_CONVP);
+  //int result = Wire.endTransmission();
+  //delay(3);
+  //// Send a command to read the ADC
+  //Wire.beginTransmission(I2C_SLAVE);
+  //Wire.write(CMD_RDADC);
+  //result = Wire.endTransmission();
+  //// Request 3 bytes of conversion results
+  //Wire.beginTransmission(I2C_SLAVE);
+  //Wire.requestFrom(I2C_SLAVE, 3);
+  //result = Wire.endTransmission();
+  //// Actually read the 3 bytes of data back and rearrange them
+  //result = Wire.available();
+  //Serial.print("Pressure read request gave ");
+  //Serial.println(result);
+  //unsigned long iPressure = 0;
+  //jndex = 0;
+  //while (Wire.available()&& jndex<3) { // slave may send less than requested
+  //  press_buff[jndex] = Wire.read(); // receive a byte as character
+  //  iPressure = (iPressure << 8) + (uint8_t) (press_buff[jndex] & 0xff);
+  //  Serial.println(press_buff[jndex]);
+  //  jndex++;
+  //}
    
   
 }
 
-void loop() {}
+void loop() {
+  unsigned long raw_pressure = 0;
+  unsigned long raw_temperature = 0;
+  if(ms5803_read_raw_pressure(&raw_pressure) != MS5803_CMD_OK){
+    errorHandler();
+  }
+  if(ms5803_read_raw_temperature(&raw_temperature) != MS5803_CMD_OK){
+    errorHandler();
+  }
+  
+  Serial.print(raw_pressure);
+  Serial.print(";");
+  Serial.println(raw_temperature);
+  delay(1000);  
+}
--- esp8285-arduino-prj/esp-m2-app-ms5803-14/esp-m2-app-ms5803-14.ino
+++ esp8285-arduino-prj/esp-m2-app-ms5803-14/esp-m2-app-ms5803-14.ino
@@ -1,5 +1,5 @@
 /*
- * app-ftp-ap-sht30-hp3038.ino
+ * esp-m2-app-ms5803-14.ino
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of
@@ -12,32 +12,44 @@
  * You should have received a copy of the GNU General Public License along with this program. If not,
  * see <https://www.gnu.org/licenses/>.
  *
- *  Created on: May 16, 2024
+ *  Created on: June 04, 2024
  *      Author: Bruno Casu
  *
- *  Version 1.0 (May 16, 2024)
+ *  Version 1.0 (June 04, 2024)
  */
  
 #include <ESP8266WiFi.h>
 #include <PolledTimeout.h>
 #include <LittleFS.h>
 #include <Wire.h>
-#include <SoftwareSerial.h>
 #include <SimpleFTPServer.h> // Replacing <ESP8266FtpServer.h>
-#include <cozir.h>
-#include <sht30.h>
-#include <LOLIN_HP303B.h>
 
 #define SERIAL_SPEED  115200
-#define APP_VERSION "app-ftp-ap-sht30-hp3038_v1.0"
+#define APP_VERSION "esp-m2-app-ms5803-14_v1.0"
 
 // Defines for the data aquisition system
-#define SHT30_I2C_ADDR_PIN_HIGH 0x45  // Jumper NOT connected
-#define SHT30_I2C_ADDR_PIN_LOW 0x44  // Jumper connected
-#define HP303B_I2C_ADDR_PIN_HIGH 0x77  // Jumper NOT connected
-#define HP303B_OVERSAMPLING_RATE  2  // From 1 to 7 (7 is  the highest oversampling rate)
-#define SLEEP_TIME_MS 1800000  // Interval between measurements in ms
+#define SLEEP_TIME_MS 1000  // Interval between measurements in ms
 #define GPIO_SET_ACCESS_POINT 14 // On Wemos D1 Mini - Pin number 14 (GPIO14)
+
+// MS5803_14 defines
+#define MS5803_CMD_OK 0
+#define MS5803_CMD_ERROR -1
+#define GPIO_2_VCC 2
+// PIN 12 in the ESP-M2 is connected to the VCC of the MS5803_14BA sensor
+// WARNING GPIO 12 MUST BE SET TO HIGH BEFORE SENSOR READING
+#define MS5803_VCC_PIN 12
+// I2C Pins
+#define I2C_SDA_PIN 4
+#define I2C_SCL_PIN 5
+// I2C Addresses
+const int16_t I2C_CONTROLLER_ADDR = 0x42;
+const int16_t MS5803_I2C_DEVICE_ADDR = 0x77;
+// MS5803 Commands
+const uint8_t CMD_RESET = 0x1E;
+const uint8_t CMD_CONVP = 0x40; // ADC conversion 256
+const uint8_t CMD_CONVT = 0x50;
+const uint8_t CMD_RDADC = 0x00;
+const uint8_t CMD_RDROM = 0xA0;
 
 // Remove when deploying in production environment
 #define DEBUG_MODE
@@ -45,32 +57,25 @@
 #define CYCLE_COUNTER_RST 10000000
 unsigned long cycle_counter = 0;
 
-// Temperature and humidity sensor
-SHT30 sht30_1_handler(SHT30_I2C_ADDR_PIN_HIGH); // Addr 0x45
-SHT30 sht30_2_handler(SHT30_I2C_ADDR_PIN_LOW); // Addr 0x44
-// Temperature and pressure sensor
-LOLIN_HP303B hp303b_handler;
-
 // WiFi Access Point configuration
 IPAddress local_IP(10,10,10,1); // FTP server address
 IPAddress gateway(10,10,10,1);
 IPAddress subnet_mask(255,255,255,0);
-const char* ssid_AP = "AP-ESP8266"; // No password set
+const char* ssid_AP = "V01-AP-ESPM2"; // No password set
 
 // FTP server access configuration
-const char* user_FTP = "esp8266";
-const char* pwd_FTP = "esp8266";
+const char* user_FTP = "espm2";
+const char* pwd_FTP = "espm2";
 FtpServer ftpSrv; // Handler
 
 // LittleFs info
 FSInfo fs_info;
 
 // Data file configuration
-const char* sht30_1_file_path = "/sht30_addr_45_data.csv";
-const char* sht30_2_file_path = "/sht30_addr_44_data.csv";
-const char* hp303b_file_path = "/hp303b_addr_77_data.csv";
-const char* sht30_csv_header_description = "Ctr;Temperature(C);RelHumidity(RH%);ExeTime(ms)"; // Added at the creation of the Data file
-const char* hp303b_csv_header_description = "Ctr;Temperature(C);Pressure(Pa);ExeTime(ms)"; // Added at the creation of the Data file
+const char* coef_file_path = "/V01_coef.txt";
+const char* coef_header_description = "C0;C1;C2;C3;C4;C5;C6;CRC;"; // Added at the creation of the Data file
+const char* data_file_path = "/V01_raw_data.csv";
+const char* data_csv_header_description = "Ctr;RawPressure;RawTemperature"; // Added at the creation of the Data file
 
 
 /** PF definitions **/
@@ -79,12 +84,9 @@ const char* hp303b_csv_header_description = "Ctr;Temperature(C);Pressure(Pa);Exe
  * Error handler function
  */
 void errorHandler(){
-  for(;;){
-    digitalWrite(LED_BUILTIN, LOW); // LED on
-    delay(100);
-    digitalWrite(LED_BUILTIN, HIGH); // LED off
-    delay(100);
-  }
+  //while(1){
+  //  delay(100);
+  //}
 }
 
 /**
@@ -144,34 +146,37 @@ void setAccessPoint() {
  */
 void createDataFiles(){
   File new_file;
+  uint16_t coef_buff[8];
   // Create Data files, if they do not exist
-  if (!LittleFS.exists(sht30_1_file_path)){ // File does not exist
-    new_file = LittleFS.open(sht30_1_file_path, "w");
+  if (!LittleFS.exists(coef_file_path)){ // File does not exist
+    new_file = LittleFS.open(coef_file_path, "w");
     if (!new_file) {
       errorHandler(); // Failed creating file
     }
     else{
-      new_file.print(sht30_csv_header_description); // Write CSV header
-      new_file.close();
+      if(ms5803_read_coefficients(coef_buff) != MS5803_CMD_OK){ // Read 8 coefficients
+        errorHandler();
+      }
+      else{
+        new_file.print(coef_header_description); // Write header
+        new_file.print("\n");
+        for(int k=0;k<8;k++){ // Write coefficients
+          new_file.print(coef_buff[k]);
+          new_file.print(";");
+        }
+        new_file.flush(); // Ensure writting before returning
+        new_file.close();
+      }
     }
   }
-  if (!LittleFS.exists(sht30_2_file_path)){ // File does not exist
-    new_file = LittleFS.open(sht30_2_file_path, "w");
+  if (!LittleFS.exists(data_file_path)){ // File does not exist
+    new_file = LittleFS.open(data_file_path, "w");
     if (!new_file) {
       errorHandler(); // Failed creating file
     }
     else{
-      new_file.print(sht30_csv_header_description); // Write CSV header
-      new_file.close();
-    }
-  }
-    if (!LittleFS.exists(hp303b_file_path)){ // File does not exist
-    new_file = LittleFS.open(hp303b_file_path, "w");
-    if (!new_file) {
-      errorHandler(); // Failed creating file
-    }
-    else{
-      new_file.print(hp303b_csv_header_description); // Write CSV header
+      new_file.print(data_csv_header_description); // Write CSV header
+      new_file.flush(); // Ensure writting before returning
       new_file.close();
     }
   }
@@ -181,36 +186,24 @@ void createDataFiles(){
  * Read and save sensor data on data files
  */
 void dataAcquisition() {
-  double hp303b_temp;
-  int32_t temp; // not used (integer value of temeprature reading)
-  int32_t hp303b_pres;
+  int p_ret, t_ret;
+  unsigned long raw_pressure = 0;
+  unsigned long raw_temperature = 0;
 
-  if (LittleFS.exists(hp303b_file_path)){ // File exists
-    hp303b_handler.begin();
-    if(hp303b_handler.measureTempOnce(temp, HP303B_OVERSAMPLING_RATE) == 0){ // Temperature reading OK
-      if(hp303b_handler.measurePressureOnce(hp303b_pres, HP303B_OVERSAMPLING_RATE) == 0){ // Pressure reading OK
-        hp303b_temp = hp303b_handler.returnDoublePrecisionTemp(); // Adapted function to retrieve decimal precision temperature
-        appendHP303BData(HP303B_I2C_ADDR_PIN_HIGH, hp303b_temp, hp303b_pres, hp303b_file_path); // Append sensor data
-      }
-    }
-  }
-  if (LittleFS.exists(sht30_1_file_path)){ // File exists
-    if(sht30_1_handler.read_single_shot() == SHT30_READ_OK){
-      appendSHT30Data(SHT30_I2C_ADDR_PIN_HIGH, sht30_1_handler.cTemp, sht30_1_handler.humidity, sht30_1_file_path); // Append sensor data
-    }
-  }
-  if (LittleFS.exists(sht30_2_file_path)){ // File exists
-    if(sht30_2_handler.read_single_shot() == SHT30_READ_OK){
-      appendSHT30Data(SHT30_I2C_ADDR_PIN_LOW, sht30_2_handler.cTemp, sht30_2_handler.humidity, sht30_2_file_path); // Append sensor data
+  if (LittleFS.exists(data_file_path)){ // File exists
+    p_ret = ms5803_read_raw_pressure(&raw_pressure);
+    t_ret = ms5803_read_raw_temperature(&raw_temperature);
+    if(p_ret == MS5803_CMD_OK && t_ret == MS5803_CMD_OK){ // Readings OK - Write data
+      appendData(raw_pressure, raw_temperature, data_file_path);
     }
   }
 }
 
 /**
- * Append the SHT30 data into the Data files
+ * Append the sensor data into the Data files
  */
-void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path) {
-  int exe_time = millis();
+void appendData(unsigned long raw_pres, unsigned long raw_temp, const char *path) {
+  //int exe_time = millis();
   File data_file = LittleFS.open(path, "a");
     if (!data_file) {
       return;
@@ -219,65 +212,167 @@ void appendSHT30Data(uint8_t i2c_addr, float temp, float r_hum, const char *path
       data_file.print("\n");
       data_file.print(cycle_counter);
       data_file.print(";");
-      data_file.print(temp, 2);
+      data_file.print(raw_pres);
       data_file.print(";");
-      data_file.print(r_hum, 2);
-      data_file.print(";");
-      data_file.print(exe_time);
+      data_file.print(raw_temp);
       data_file.flush(); // Ensure writting before returning
     }
     data_file.close();
 #ifdef DEBUG_MODE
-    Serial.print("\n-->SHT30 ");
-    Serial.print(i2c_addr, HEX);
-    Serial.print(" T(C): ");
-    Serial.print(temp);
-    Serial.print(" RH(RH%): ");
-    Serial.print(r_hum);
-    Serial.print(" ExeTime(ms): ");
-    Serial.print(exe_time);
+    Serial.print("\n-->DATA");
+    Serial.print(" P: ");
+    Serial.print(raw_pres);
+    Serial.print(" T: ");
+    Serial.print(raw_temp);
 #endif // DEBUG_MODE
 }
 
+// MS5803_14 Sensor functions
 /**
- * Append the HP303B data into the Data files
+ * Send I2C command
+ * Return 0 if Tx OK, -1 if Error
  */
-void appendHP303BData(uint8_t i2c_addr, double temp, int32_t pres, const char *path) {
-  int exe_time = millis();
-  File data_file = LittleFS.open(path, "a");
-    if (!data_file) {
-      return;
+int ms5803_send_cmd(uint8_t cmd){
+  Wire.beginTransmission(MS5803_I2C_DEVICE_ADDR);
+  Wire.write(cmd);
+  delay(3);
+  if (Wire.endTransmission() == 0) {
+    return 0; // Command response OK
+  }
+  else {
+    return -1; // Error in I2C transmission
+  }
+}
+
+/**
+ * Initialyze the MS5803 sensor: configures the GPIOs and send a Reset command
+ * Return 0 if command sent is OK, and -1 if Error
+ */
+int ms5803_init(void){
+  pinMode(MS5803_VCC_PIN, OUTPUT);
+  digitalWrite(MS5803_VCC_PIN, HIGH); // Must be set to high to power the ms503 sensor
+  pinMode(GPIO_2_VCC, OUTPUT);
+  digitalWrite(GPIO_2_VCC, HIGH);
+  //delay(100);
+  Wire.setClock(100000UL);
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN, I2C_CONTROLLER_ADDR);
+  // Send a RESET command
+  if (ms5803_send_cmd(CMD_RESET) != MS5803_CMD_OK){
+    return -1;
+  }
+  else{
+    return 0;
+  }
+}
+
+/**
+ * Read the MS5803 coefficients from the ROM memory
+ * Return 0 if command sent is OK, and -1 if Error
+ */
+int ms5803_read_coefficients(uint16_t *coef){
+  int n;
+  uint8_t buff[2];
+  for (int i = 0; i < 8; i++) { // Request values for 8 coefficients
+    if (ms5803_send_cmd((uint8_t) (CMD_RDROM + (i << 1))) != MS5803_CMD_OK){
+      return -1;
     }
-    else {
-      data_file.print("\n");
-      data_file.print(cycle_counter);
-      data_file.print(";");
-      data_file.print(temp, 2);
-      data_file.print(";");
-      data_file.print(pres);
-      data_file.print(";");
-      data_file.print(exe_time);
-      data_file.flush(); // Ensure writting before returning
+    n = 0;
+    buff[0]=0;
+    buff[1]=0;
+    Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 2); // Request 2 bytes from device
+    delay(1);
+    while (Wire.available() && n < 2) { // Device may send less than requested
+      buff[n] = Wire.read();
+      n++;
     }
-    data_file.close();
-#ifdef DEBUG_MODE
-    Serial.print("\n-->HP303B ");
-    Serial.print(i2c_addr, HEX);
-    Serial.print(" T(C): ");
-    Serial.print(temp);
-    Serial.print(" P(Pa): ");
-    Serial.print(pres);
-    Serial.print(" ExeTime(ms): ");
-    Serial.print(exe_time);
-#endif // DEBUG_MODE
+    if (n != 2) {
+      return -1; // Error in I2C transmission
+    }
+    // Copy 16 bit coefficient
+    coef[i] = (uint16_t)buff[1] | ((uint16_t)buff[0] << 8);
+    delay(5);
+  }
+  return 0;
+}
+
+/**
+ * Send command to trigger the ADC conversion then request the Raw data (3 bytes)
+ * Return raw data value
+ */
+int ms5803_read_raw_pressure(unsigned long *raw_pressure){
+  int n;
+  uint8_t buff[3];
+  *raw_pressure = 0;
+  // Send Conversion request for Pressure
+  if (ms5803_send_cmd(CMD_CONVP) != MS5803_CMD_OK){
+    return -1;
+  }
+
+  delay(3); // Delay for conversion
+
+  // Send read ADC command
+  if (ms5803_send_cmd(CMD_RDADC) != MS5803_CMD_OK){
+    return -1;
+  }
+
+  Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 3);
+  delay(1);
+  n = 0;
+  while (Wire.available() && n < 3) { // slave may send less than requested
+    buff[n] = Wire.read(); // receive a byte as character
+    *raw_pressure = (*raw_pressure << 8) + (uint8_t) (buff[n] & 0xff);
+    n++;
+  }
+  if (n != 3) {
+    return -1; // Error in I2C transmission
+  }
+  else{
+    return 0; // Command OK
+  }
+
+}
+
+/**
+ * Send command to trigger the ADC conversion then request the Raw data (3 bytes)
+ * Return raw data value
+ */
+int ms5803_read_raw_temperature(unsigned long *raw_temperature){
+  int n;
+  uint8_t buff[3];
+  *raw_temperature = 0;
+  // Send Conversion request for Pressure
+  if (ms5803_send_cmd(CMD_CONVT) != MS5803_CMD_OK){
+    return -1;
+  }
+
+  delay(3); // Delay for conversion
+
+  // Send read ADC command
+  if (ms5803_send_cmd(CMD_RDADC) != MS5803_CMD_OK){
+    return -1;
+  }
+
+  Wire.requestFrom(MS5803_I2C_DEVICE_ADDR, 3);
+  delay(1);
+  n = 0;
+  while (Wire.available() && n < 3) { // slave may send less than requested
+    buff[n] = Wire.read(); // receive a byte as character
+    *raw_temperature = (*raw_temperature << 8) + (uint8_t) (buff[n] & 0xff);
+    n++;
+  }
+  if (n != 3) {
+    return -1; // Error in I2C transmission
+  }
+  else{
+    return 0; // Command OK
+  }
+
 }
 
 
 void setup() {
   system_rtc_mem_read(64, &cycle_counter, 4); // Copy RTC memory value in current cycle counter
   if (cycle_counter > CYCLE_COUNTER_RST){cycle_counter = 0;} // This prevents errors in first boot if done by GPIO reset (RTC memeory gets wrong value)
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // LED off
   pinMode(GPIO_SET_ACCESS_POINT, INPUT_PULLUP); // Set Access point pin
   Wire.begin(); // I2C
   WiFi.mode(WIFI_OFF); // Must turn the modem off; using disconnect won't work
@@ -289,7 +384,10 @@ void setup() {
 #ifdef DEBUG_MODE
   printDeviceInfo();
 #endif // DEBUG_MODE  
-
+  // Initialyze MS5803_14
+  if(ms5803_init() != MS5803_CMD_OK){
+    errorHandler();
+  }
   // Setup data files and read the sensors
   createDataFiles();
   dataAcquisition();
